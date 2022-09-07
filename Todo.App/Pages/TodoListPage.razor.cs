747b761faaeeff3f7e631cc72fea52f3194e967f
ChristopheDEBOVE <cdebove15@gmail.com>
2022-09-07 19:49:22 +0200

    Drag drop Trash

--- Todo.App/Pages/TodoListPage.razor.cs
+++ Todo.App/Pages/TodoListPage.razor.cs
@@ -1,18 +1,34 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Todo.Models;
 
-namespace Todo.Pages;
-
+namespace Todo.Pages; 
+    
 public partial class TodoListPage
 {
-
+    [Inject] private IJSRuntime JsRuntime { get; set; }
+    private IJSObjectReference _jsModule;
+    
+    protected override async Task OnInitializedAsync()
+    { 
+        _jsModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/TodoListPage.razor.js");
+        await _jsModule.InvokeVoidAsync("SetTodoListPageObjReference", DotNetObjectReference.Create(this));
+        await base.OnInitializedAsync();
+    }
+ 
     [Inject]
     public TodoList TodoList { get; set; }
-
     private string _newItem = String.Empty;
     
     private void AddItem()
     {
         TodoList.Add(_newItem);
+    }
+
+    [JSInvokable]
+    public void Remove(string taskId)
+    {
+        TodoList.Remove(Guid.Parse(taskId)); 
+        StateHasChanged();
     }
 }
---
+++ Todo.App/Pages/TodoListPage.razor.js
@@ -0,0 +1,29 @@
+﻿// Events fired on the drag target
+let todoListPageObjReference = null;
+export function SetTodoListPageObjReference(reference){
+    todoListPageObjReference = reference;
+}
+document.addEventListener("dragstart", function(event) {
+    event.dataTransfer.setData("todoItemId",event.target.getAttribute("data-todoItemId") ); 
+});
+
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+
+/* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+Reset the color of the output text and DIV's border color
+Get the dragged data with the dataTransfer.getData() method
+The dragged data is the id of the dragged element ("drag1")
+Append the dragged element into the drop element
+*/
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    if ( event.target.className == "droptarget" ) {
+        var todoItemId = event.dataTransfer.getData("todoItemId");
+        console.log(todoItemId);
+        todoListPageObjReference.invokeMethodAsync("Remove",todoItemId);
+    }
+});
