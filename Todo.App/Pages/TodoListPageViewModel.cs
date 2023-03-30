ccda98744a5ed18ee4da529b3955cead60cf12a2
benoit <lacour.benoit@gmail.com>
2023-03-30 09:04:36 +0200

    code cleanup

--- Todo.App/Pages/TodoListPageViewModel.cs
+++ Todo.App/Pages/TodoListPageViewModel.cs
@@ -5,6 +5,7 @@ namespace Todo.Pages;
 
 public class TodoListPageViewModel: ReactiveObject
 {
+    
     private string _search = string.Empty;
     private IEnumerable<TodoItem> _todoItems = Enumerable.Empty<TodoItem>();
     private readonly ObservableAsPropertyHelper<IEnumerable<TodoItem>> _filteredTodoItems;
@@ -32,7 +33,7 @@ public class TodoListPageViewModel: ReactiveObject
                     x=>x.TodoItems, 
                 (search, items) 
                     => items.Where(x => x.Label.ToLower().Contains(search.ToLower())))
-            .ToProperty(this, x => x.FilteredTodoItems, initialValue: TodoItems);
+                .ToProperty(this, x => x.FilteredTodoItems, initialValue: TodoItems);
 
         AddNewItemCommand = ReactiveCommand.Create<string, IEnumerable<TodoItem>>(label => TodoItems.Append(new TodoItem(){Id = Guid.NewGuid() ,Label = label}));
 
